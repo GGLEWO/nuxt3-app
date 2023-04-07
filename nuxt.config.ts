7e8d8629e6fd4e4793b83c033d84e2f468f7c780
guanyaoming <guanyaoming@linklogis.com>
2023-04-07 12:04:38 +0800

    fix: 修改网站信息

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: guanyaoming guanyaoming@linklogis.com
  * @Date: 2023-03-27 14:58:18
- * @LastEditors: guanym 1195817329@qq.com
- * @LastEditTime: 2023-04-06 21:26:34
+ * @LastEditors: guanyaoming guanyaoming@linklogis.com
+ * @LastEditTime: 2023-04-07 12:03:44
  * @FilePath: \nuxt3-app\nuxt.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -16,10 +16,21 @@
  */
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  title: 'ymtool',
-  description: 'ym 个人博客'
   app: {
     baseURL: '/nuxt',
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
+      title: 'ym',
+      htmlAttrs: {
+        lang: 'zh'
+      },
+      meta: [
+        { name: 'content-type', content: 'text/html;charset=utf-8' },
+        { name: 'keywords', content: 'ym 个人博客'},
+        { name: 'description', content:  'ym 个人博客'}
+      ],
+    }
   },
   build: { transpile: [/vant/] },
   modules: ['@nuxtjs/tailwindcss'],
