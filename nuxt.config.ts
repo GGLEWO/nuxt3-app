19089f6d6fd920bc59b225407e063bb675a31e45
guanyaoming <guanyaoming@linklogis.com>
2023-04-10 11:32:57 +0800

    fix: 更改预览样式

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
  * @Author: guanyaoming guanyaoming@linklogis.com
  * @Date: 2023-03-27 14:58:18
  * @LastEditors: guanyaoming guanyaoming@linklogis.com
- * @LastEditTime: 2023-04-07 12:03:44
+ * @LastEditTime: 2023-04-10 11:10:33
  * @FilePath: \nuxt3-app\nuxt.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -55,7 +55,7 @@ export default defineNuxtConfig({
     },
     server: {
       proxy: {
-        '/api': {
+        '/nuxt/api': {
           target: 'http://www.tmtool.com.cn', // 目标接口
           changeOrigin: true,
         },
--- plugins/markdown.js
+++ plugins/markdown.js
@@ -2,18 +2,21 @@
  * @Author: guanyaoming guanyaoming@linklogis.com
  * @Date: 2023-03-31 20:51:20
  * @LastEditors: guanyaoming guanyaoming@linklogis.com
- * @LastEditTime: 2023-03-31 21:58:22
+ * @LastEditTime: 2023-04-10 11:31:54
  * @FilePath: \nuxt3-app\plugins\markdown.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import VMdPreview from '@kangc/v-md-editor/lib/preview'
-import '@kangc/v-md-editor/lib/style/preview.css'
-import githubTheme from '@kangc/v-md-editor/lib/theme/github.js'
-import '@kangc/v-md-editor/lib/theme/style/github.css'
+import '@kangc/v-md-editor/lib/style/base-editor.css'
+import githubTheme from '@kangc/v-md-editor/lib/theme/vuepress.js'
+import '@kangc/v-md-editor/lib/theme/style/vuepress.css'
 import VueHighlightJS from 'vue3-highlightjs'
 import 'vue3-highlightjs/styles/vs2015.css'
+import Prism from 'prismjs'
 
-VMdPreview.use(githubTheme)
+VMdPreview.use(githubTheme, {
+  Prism,
+})
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueHighlightJS)
