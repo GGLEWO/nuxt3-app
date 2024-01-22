9c5fbee5a7a5166723cdbc22fd65919be6bb0455
guanyaoming <guanyaoming@linklogis.com>
2024-01-22 15:57:07 +0800

    fix: 修改配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,3 @@
-/*
- * @Author: guanyaoming guanyaoming@linklogis.com
- * @Date: 2023-03-27 14:58:18
- * @LastEditors: guanyaoming guanyaoming@linklogis.com
- * @LastEditTime: 2023-04-10 11:10:33
- * @FilePath: \nuxt3-app\nuxt.config.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-/*
- * @Author: guanyaoming guanyaoming@linklogis.com
- * @Date: 2023-03-27 14:58:18
- * @LastEditors: guanyaoming guanyaoming@linklogis.com
- * @LastEditTime: 2023-04-03 17:44:54
- * @FilePath: \nuxt3-app\nuxt.config.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
@@ -56,9 +40,10 @@ export default defineNuxtConfig({
     server: {
       proxy: {
         '/nuxt/api': {
-          target: 'http://www.tmtool.com.cn', // 目标接口
+          target: 'http://localhost:3001/',
           changeOrigin: true,
-        },
+          rewrite: (path) => path.replace(/^\/nuxt/, '')
+        }
       },
     }
   },
