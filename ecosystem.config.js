26f7e87e58936fbfbeb95892d7b3cafbe045c197
guanyaoming <guanyaoming@linklogis.com>
2024-01-22 16:54:42 +0800

    style: 统一样式

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@
  * @Author: guanyaoming guanyaoming@linklogis.com
  * @Date: 2023-03-30 14:20:39
  * @LastEditors: guanyaoming guanyaoming@linklogis.com
- * @LastEditTime: 2023-03-31 11:04:13
+ * @LastEditTime: 2024-01-22 16:52:52
  * @FilePath: \nuxt3-app\ecosystem.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -15,8 +15,8 @@ module.exports = {
       instances: '3',
       script: './.output/server/index.mjs',
       env: {
-        "PORT": 8588
-      },
+        PORT: 8588
+      }
     }
   ]
 }
